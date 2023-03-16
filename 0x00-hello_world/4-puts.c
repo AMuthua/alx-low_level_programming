424913634038d0438276e691761882beac36ecfd
Alexander <alexandermuthua@gmail.com>
2023-03-16 15:17:56 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - this is the main entry point of the C code
+ * return 0 - indicates a program is successful
+*/
+
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle")
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
