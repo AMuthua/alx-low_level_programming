b242918f2ad2fe8089fcede22b2de1eedf232100
Alexander <alexandermuthua@gmail.com>
2023-03-17 04:47:35 -0700

    UNIX is simple, only understand it.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point 
+ * main - A program that prints a line to the standard error
  * return: Always 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpiar, 2015-10-19\n", 59);
 	return (1);
 }
