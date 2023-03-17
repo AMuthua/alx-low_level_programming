49c94ecd3f437f39bb631ff2956c55af6c1f37c3
Alexander <alexandermuthua@gmail.com>
2023-03-17 15:19:18 -0700

    Working with base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Description - Printing single digit numbers of base 10.
+ * Return: 0 always (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n)
+	}
+	printf("\n");
+	return (0);
+}
