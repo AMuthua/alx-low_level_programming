75ccf5a7c4b1725dac6289d09bafa98d09dcade6
Alexander <alexandermuthua@gmail.com>
2023-05-01 05:46:53 -0700

    Updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t z = 0;
+	size_t nnodes = 0;
 
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		z++;
+		nnodes++;
 	}
-	return (z);
+	return (nnodes);
 }
